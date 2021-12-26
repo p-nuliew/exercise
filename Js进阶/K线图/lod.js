989636b89310c9d30c6a71333747299c3add7202
chenweilun <chenwl@shinemo.com>
2021-12-26 19:23:55 +0800

    pref: 封装逻辑优化

--- Js进阶/K线图/lod.js
+++ Js进阶/K线图/lod.js
@@ -1,68 +1,27 @@
-// javascript
-const canvas = document.getElementById('canvas');
-canvas.style.background = 'pink'
 
-// 最高价、最低价、开盘价、收盘价
-const data = [
-  { heightPrice: 1000, lowPrice: 500, openingPrice: 800, closingPice: 900 },
-  { heightPrice: 1000, lowPrice: 625, openingPrice: 800, closingPice: 700 },
-  { heightPrice: 1000, lowPrice: 750, openingPrice: 800, closingPice: 900 },
-  { heightPrice: 905, lowPrice: 625, openingPrice: 701, closingPice: 903 },
-  { heightPrice: 1000, lowPrice: 550, openingPrice: 807, closingPice: 709 },
-  { heightPrice: 1000, lowPrice: 800, openingPrice: 807, closingPice: 909 },
-  { heightPrice: 1000, lowPrice: 600, openingPrice: 807, closingPice: 909 },
-  { heightPrice: 1000, lowPrice: 600, openingPrice: 900, closingPice: 908 },
-  { heightPrice: 904, lowPrice: 600, openingPrice: 701, closingPice: 803 },
-  { heightPrice: 1000, lowPrice: 600, openingPrice: 807, closingPice: 909 },
-]
 
 if (canvas.getContext) {
   ctx = canvas.getContext('2d');
-  renderKLineChart(data, {
-    yAxisSplitNumber: 4,
-    space: 60,
-    triangleH: 10,
-    tickWidth: 5,
-    xAxisTickSpace: 40,
-    candleW: 10,
-    curveType: 'lowPrice'
-  })
-}
 
-/**
- * 绘制k线图
- * @param {array} data 数据源
- * @param {config} config k线图配置项
- */
-function renderKLineChart (
-  data,
-  config = {
-    // y轴分段数量
-    yAxisSplitNumber: 4,
-    // 坐标轴与容器间的边距
-    space: 60,
-    // 三角形高度
-    triangleH: 10,
-    // 刻度线宽度
-    tickWidth: 5,
-    // x轴刻度间距
-    xAxisTickSpace: 40,
-    // 蜡烛宽度
-    candleW: 10,
-    // 曲线类型
-    curveType: 'lowPrice'
-  }
-) {
-  const { yAxisSplitNumber, space, triangleH, tickWidth, xAxisTickSpace, candleW, curveType } = config
   // 已知条件
+  // 坐标轴与容器间的边距
+  const space = 60
   // 容器宽度
   const width = ctx.canvas.width
   // 容器高度
   const height = ctx.canvas.height
+  // 三角形高度
+  const triangleH = 10
+  // 刻度线宽度
+  const tickWidth = 5
   // x轴刻度数量
   const xAxisTickCount = data.length
 
-  // 可知条件
+
+  // x轴刻度间距
+  const xAxisTickSpace = 40
+  // 蜡烛宽度
+  const candleW = 10
   // 原点横坐标
   const originX = space
   // 原点纵坐标
@@ -75,16 +34,40 @@ function renderKLineChart (
   const xAxisVertexX = width - space
   // x轴顶点纵坐标
   const xAxisVertexY = height - space
+
+  // 曲线类型
+  const curveType = 'lowPrice'
+
   // y轴高度
   const yAxisHeight = height - space * 2
+  console.log('yAxisHeight: ', yAxisHeight);
+  // y轴分段数量
+  const yAxisSplitNumber = 4
   // y轴刻度间距
   const yAxisTickSpace = yAxisHeight / yAxisSplitNumber
   // 最高价最大的值
   const maxPrice = Math.max(...data.map(x => x.heightPrice))
-
+  const minPrice = Math.min(...data.map(x => x.lowPrice))
+  console.log('minPrice: ', minPrice);
+  const splitBase = (maxPrice - minPrice) / yAxisSplitNumber
   // 价格与y轴高度的比率
   const yAxisPriceRate = maxPrice / yAxisHeight
-  // 纵坐标集合
+  console.log('yAxisPriceRate: ', yAxisPriceRate);
+
+  // 实际价格转为纵坐标
+  const tranPriceToOrdinate = price => originY - price / yAxisPriceRate
+  // Y轴刻度文字
+  const yAxisTickText = i => (yAxisTickSpace * i * yAxisPriceRate).toFixed(2)
+
+  // TODO Y轴刻度和纵坐标优化
+  // const yAxisTickText = i => (minPrice + splitBase * i).toFixed(2)
+  // 不知道哪里计算错误
+  // const tranPriceToOrdinate = price => originY - price / yAxisPriceRate + (price - minPrice) / yAxisPriceRate
+
+
+  console.log('maxPrice: ', maxPrice);
+
+  // 数据纵坐标
   const dataYAxisPoint = data.map(it => {
     const newIt = {}
     for (key in it) {
@@ -93,27 +76,40 @@ function renderKLineChart (
     return newIt
   })
 
-  // TODO Y轴刻度和纵坐标优化
-  // const minPrice = Math.min(...data.map(x => x.lowPrice))
-  // const splitBase = (maxPrice - minPrice) / yAxisSplitNumber
-  // const yAxisTickText = i => (minPrice + splitBase * i).toFixed(2)
-  // 不知道哪里计算错误
-  // const tranPriceToOrdinate = price => originY - price / yAxisPriceRate + (price - minPrice) / yAxisPriceRate
+  // x轴刻度横坐标
+  xAxisTickPointX = i => originX + i * xAxisTickSpace
 
-
-  // 绘制Y轴
+  // 1.2 绘制Y轴
   ctx.beginPath()
   ctx.moveTo(originX, originY)
   ctx.lineTo(yAxisVertexX, yAxisVertexY)
   ctx.closePath();
   ctx.stroke()
 
-  // 绘制x轴
+  // 1.3 绘制x轴
   ctx.beginPath()
   ctx.moveTo(originX, originY)
   ctx.lineTo(xAxisVertexX, xAxisVertexY)
   ctx.closePath();
   ctx.stroke()
+
+  // /**
+  //  * 绘制y轴三角形
+  //  * @param {number} yAxisVertexX y轴顶点横坐标
+  //  * @param {number}} yAxisVertexY y轴顶点纵坐标
+  //  * @param {number}} triangleH 三角形高度
+  //  */
+  //  function renderYAxisTriangle (yAxisVertexX, yAxisVertexY, triangleH) {
+  //   ctx.beginPath()
+  //   ctx.moveTo(yAxisVertexX, yAxisVertexY)
+  //   ctx.lineTo(yAxisVertexX - triangleH/2, yAxisVertexY + triangleH)
+  //   ctx.lineTo(yAxisVertexX + triangleH/2, yAxisVertexY + triangleH)
+  //   ctx.fill()
+  // }
+  // // TODO 这里是否有必要把已知变量当做参数传入？
+  // // 好处：把变量控制在函数作用域内，防止污染外部变量。坏处：感觉多此一举
+  // renderYAxisTriangle(yAxisVertexX, yAxisVertexY, triangleH)
+
 
   // 绘制y轴三角形
   ctx.beginPath()
@@ -145,28 +141,6 @@ function renderKLineChart (
     const xAxisTickX = xAxisTickPointX(i)
     renderText(i, xAxisTickX, originY + tickWidth + 10, 'center')
     renderLine(xAxisTickX, originY, xAxisTickX, originY + tickWidth)
-  }
-
-  // 绘制一串蜡烛
-  renderCandles(data, candleW)
-
-  // 绘制贝塞尔曲线
-  renderBezierCurve(getControlPointInfo(curveType))
-
-
-  // x轴刻度横坐标
-  function xAxisTickPointX (i) {
-    return originX + i * xAxisTickSpace
-  }
-
-  // 实际价格转为纵坐标
-  function tranPriceToOrdinate (price) {
-    return originY - price / yAxisPriceRate
-  }
-
-  // Y轴刻度文字
-  function yAxisTickText (i) {
-    return (yAxisTickSpace * i * yAxisPriceRate).toFixed(2)
   }
 
   /**
@@ -232,13 +206,14 @@ function renderKLineChart (
       ctx.stroke()
     }
   }
+  renderCandles(data, 10)
 
-   /**
+  /**
    * 获取当前点以及前后控制点坐集合
-   * @param {string} curveType 曲线类型 { heightPrice, lowPrice, openingPrice, closingPice }
    * @returns [array] 当前点以及前后控制点坐集合
    */
-  function getControlPointInfo (curveType) {
+  // 注意，不要与价格强关联
+  const getControlPointInfo = () => {
     let controlPoint = []
 
     for (let i = 0; i < xAxisTickCount; i++) {
@@ -303,11 +278,11 @@ function renderKLineChart (
     return controlPoint
   }
 
-   /**
+  /**
    * 绘制贝塞尔曲线
    * @param {array} controlPoint 控制点集合:  [{ curX: lowPricePointX, curY: lowPricePointY, prevControlX, prevControlY, nextControlX, nextControlY } ...]
    */
-  function renderBezierCurve (controlPoint) {
+  const renderBezierCurve = controlPoint => {
     ctx.beginPath();
     for (let i = 0; i < controlPoint.length; i++) {
       const {
@@ -319,6 +294,11 @@ function renderKLineChart (
 
       if (i > 0 && i < controlPoint.length) {
         const prevNode = controlPoint[i - 1]
+        // 验证控制点是否正确
+        // ctx.lineTo(prevNode.nextControlX, prevNode.nextControlY)
+        // ctx.lineTo(prevControlX, prevControlY)
+        // ctx.lineTo(curX, curY)
+
         ctx.bezierCurveTo(prevNode.nextControlX, prevNode.nextControlY, prevControlX, prevControlY, curX, curY);
       } else if ( i === 0) {
         ctx.moveTo(curX, curY);
@@ -326,35 +306,72 @@ function renderKLineChart (
     }
     ctx.stroke();
   }
+  renderBezierCurve(getControlPointInfo())
+}
 
-  /**
-   * 绘制线条
-   * @param {sx} sx 开始坐标点横坐标
-   * @param {sy} sy 开始坐标点纵坐标
-   * @param {ex} ex 结束坐标点横坐标
-   * @param {ey} ey 结束坐标点纵坐标
-   */
-  function renderLine (sx, sy, ex, ey) {
-    ctx.beginPath()
-    ctx.moveTo(sx, sy)
-    ctx.lineTo(ex, ey)
-    ctx.closePath();
-    ctx.stroke()
-  }
 
-  /**
-   * 轴线文字
-   * @param {string} text 文本
-   * @param {number} x 横坐标
-   * @param {number} y 纵坐标
-   * @param {string} textAlign 文本对齐方式
-   */
-  function renderText (text, x, y, textAlign) {
-    ctx.fillStyle = "#FF0000";  // 文字颜色
-    ctx.textBaseline = "middle";
-    ctx.textAlign = textAlign;
 
-    ctx.font = "10px Arial";  // 高为10px的字体
-    ctx.fillText(text, x, y)  // 描绘实体文字
+
+
+// TODO
+// 1. 处理边界问题：以补全的方式、虚拟的点，让边界问题变成常规问题
+// 2. 数据整合：整合成我们需要的ui数据
+// 3. 封装
+// 原点不应该是0,0
+
+
+// 以视图（折线）的方式验证结果（前后控制点）是否正确
+// 最高价发生变动时，y轴刻度文字、蜡烛和曲线需要响应变化
+//   刻度文字*倍率，（蜡烛和曲线）/倍率
+
+
+// 面向对象
+// 父类
+class Person {
+	constructor(sex, age) {
+  	this.sex = sex
+    this.age = age
   }
 }
+
+// 子类
+class Student extends Person {
+	constructor(studentNumber, sex, age) {
+    super(sex, age)
+  	this.studentNumber = studentNumber
+
+    // 继承Person的属性
+    // this.sex = sex
+    // this.age = age
+  }
+}
+
+student1 = new Student('330311221', '男', 15)
+console.log('student1: ', student1);
+
+
+// 封装理念整理：
+// 1.先有场景，再封装，先有子类，再有父类
+// 2. 封装颗粒度：取决于场景的偏向性
+// 3。 封装偏向性：工具函数的独立性，结合场景分析
+
+
+// 代码待完善：
+// 1. 绘制一串蜡烛的函数
+// 2. 传入一个参数，可以绘制不同的曲线
+// 3. 起点价格的定位
+// 4. 实现辅助线，并思考“变和不变”上的优化点
+// 5. 实现拖拽功能，
+
+// const data = []
+// function renderFn (
+//   data,
+//   config = {
+//     candleBgColor: ''
+//   },
+// ) {
+
+// }
+
+
+
