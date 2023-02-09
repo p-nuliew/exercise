b4b1f138e11ffca413eba8fc9e5fa59bb4eaa0cf
weilun0510 <13566106399@163.com>
2023-02-09 16:25:30 +0800

    算法:列表转树

--- 算法/冒泡排序/index.js
+++ 算法/冒泡排序/index.js
@@ -16,3 +16,22 @@ function sort3(arr) {
 }
 
 console.log(sort3([1, 3, 2, 5, 4]));
+
+
+
+// 1. 比较前后大小，如果前面比后面大，则交换位置。每次遍历完，最大值肯定在末尾。遍历length-1次。
+function sort4(arr) {
+  const length = arr.length;
+  let done = true;  // 用于优化，如果某次遍历不存在前者大于后者，说明排序已完成
+  for(let i = 0; i < length - 1; i++) {
+    for (let j = 0; j < length - i - 1; j++) {
+      if (arr[j] > arr[j+1]) {
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+        done = false
+      }
+    }
+    if (done) break;
+  }
+  return arr
+}
+console.log(sort4([1, 3, 222, 5, 4]));
--- 算法/快排/index.js
+++ 算法/快排/index.js
@@ -1,24 +1,49 @@
-var arr1 = [1, 6, 63, 5];
-function quickSort(arr) {
-  var arrLength = arr.length;
-  console.log("arrLength: ", arrLength);
-  if (arrLength <= 1) return arr;
-  var index = Math.floor(arrLength / 2);
-  var base = arr.splice(index, 1)[0];
-  var left = [];
-  var right = [];
+// 采用分治思想：
+//   选择一个基准值（可以为中间值，或首尾值），建立两个数组left和right。
+//   遍历整个数组，比基准值小的扔进left，大的扔进right
+//   遍历完之后，再重复以上步骤，直到left/right只剩一个值，然后返回该值
 
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] < base) {
-      left.push(arr[i]);
+// 快速排序是在冒泡排序的基础上改进而来的。
+// 快速排序基本上被认为是在同数量级的所有排序算法中，平均性能最好的
+
+var arr1 = [1, 6, 63, 5];
+// function quickSort(arr) {
+//   var arrLength = arr.length;
+//   console.log("arrLength: ", arrLength);
+//   if (arrLength <= 1) return arr;
+//   var index = Math.floor(arrLength / 2);
+//   var base = arr.splice(index, 1)[0];
+//   var left = [];
+//   var right = [];
+
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] < base) {
+//       left.push(arr[i]);
+//     } else {
+//       right.push(arr[i]);
+//     }
+//   }
+//   return quickSort(left).concat([base], quickSort(right));
+// }
+// console.log(quickSort(arr1));
+
+const quickSort2 = (arr) => {
+  if (arr.length <= 1) return arr;
+  const midIndex = Math.floor(arr.length / 2);
+  const mid = arr.splice(midIndex, 1)[0];
+  const left = [];
+  const right = [];
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i]
+    if (item < mid) {
+      left.push(item)
     } else {
-      right.push(arr[i]);
+      right.push(item)
     }
   }
-  return quickSort(left).concat([base], quickSort(right));
+  return quickSort2(left).concat([mid], quickSort2(right))
 }
-
-console.log(quickSort(arr1));
+console.log('quickSort2', quickSort2(arr1));
 
 // var arr2 = [3, 34];
 // function qs(arr) {
